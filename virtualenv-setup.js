a21c4b626a97ad183704188717f6bf63f3e81f6f
Mo Laiouny <mlaiouny@gmail.com>
2018-05-13 10:44:06 -0400

    Initial commit

---
+++ virtualenv-setup.js
@@ -0,0 +1,18 @@
+// Installing Virtualenv with pip
+// easy_install pip3
+// The next step is to install the virtualenv package
+// pip3 install virtualenv
+// The next step is to create the environment with virtualenv
+// virtualenv my_blog_environment
+// The next step is to activate your virtual environment
+// cd my_blog_environment
+// source bin/activate
+// To see all the contents in the my_blog_environment folder
+// ls
+//bin include lib
+// Virtual Sandboxes
+// Another interesting feautre of virtualenv is the ability to create sandboxes for different
+// versions of python interpreter with the -p flag
+// virtualenv --no-site-packages -p/user/bin/python3 my_blog_environment
+// cd my_blog_environment
+// source bin/activate
